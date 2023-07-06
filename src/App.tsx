1b78e929f39e5fa36d1dfd7e5adf51e030e320e5
jatin <rjjatinrawat9@gmail.com>
2023-07-06 12:46:10 +0530

    "Initial commit from Create Refine App"

---
+++ src/App.tsx
@@ -0,0 +1,399 @@
+import {
+    GitHubBanner,
+    Refine,
+    AuthBindings,
+    Authenticated,
+} from "@refinedev/core";
+import {
+    notificationProvider,
+    ThemedLayoutV2,
+    ErrorComponent,
+    AuthPage,
+    RefineThemes,
+} from "@refinedev/antd";
+import { dataProvider, liveProvider } from "@refinedev/supabase";
+import routerProvider, {
+    CatchAllNavigate,
+    NavigateToResource,
+    UnsavedChangesNotifier,
+    DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+import { ConfigProvider, notification } from "antd";
+import { GoogleOutlined } from "@ant-design/icons";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+
+import "@refinedev/antd/dist/reset.css";
+
+import { PostList, PostCreate, PostEdit, PostShow } from "../src/pages/posts";
+import { supabaseClient } from "../src/utility";
+
+const authProvider: AuthBindings = {
+    login: async ({ email, password, providerName }) => {
+        // sign in with oauth
+        try {
+            if (providerName) {
+                const { data, error } =
+                    await supabaseClient.auth.signInWithOAuth({
+                        provider: providerName,
+                    });
+
+                if (error) {
+                    return {
+                        success: false,
+                        error,
+                    };
+                }
+
+                if (data?.url) {
+                    return {
+                        success: true,
+                    };
+                }
+            }
+
+            // sign in with email and password
+            const { data, error } =
+                await supabaseClient.auth.signInWithPassword({
+                    email,
+                    password,
+                });
+
+            if (error) {
+                return {
+                    success: false,
+                    error,
+                };
+            }
+
+            if (data?.user) {
+                return {
+                    success: true,
+                };
+            }
+        } catch (error: any) {
+            return {
+                success: false,
+                error,
+            };
+        }
+
+        return {
+            success: false,
+            error: {
+                message: "Login failed",
+                name: "Invalid email or password",
+            },
+        };
+    },
+    register: async ({ email, password }) => {
+        try {
+            const { data, error } = await supabaseClient.auth.signUp({
+                email,
+                password,
+            });
+
+            if (error) {
+                return {
+                    success: false,
+                    error,
+                };
+            }
+
+            if (data) {
+                return {
+                    success: true,
+                };
+            }
+        } catch (error: any) {
+            return {
+                success: false,
+                error,
+            };
+        }
+
+        return {
+            success: false,
+            error: {
+                message: "Register failed",
+                name: "Invalid email or password",
+            },
+        };
+    },
+    forgotPassword: async ({ email }) => {
+        try {
+            const { data, error } =
+                await supabaseClient.auth.resetPasswordForEmail(email, {
+                    redirectTo: `${window.location.origin}/update-password`,
+                });
+
+            if (error) {
+                return {
+                    success: false,
+                    error,
+                };
+            }
+
+            if (data) {
+                notification.open({
+                    type: "success",
+                    message: "Success",
+                    description:
+                        "Please check your email for a link to reset your password. If it doesn't appear within a few minutes, check your spam folder.",
+                });
+                return {
+                    success: true,
+                };
+            }
+        } catch (error: any) {
+            return {
+                success: false,
+                error,
+            };
+        }
+
+        return {
+            success: false,
+            error: {
+                message: "Forgot password failed",
+                name: "Invalid email",
+            },
+        };
+    },
+    updatePassword: async ({ password }) => {
+        try {
+            const { data, error } = await supabaseClient.auth.updateUser({
+                password,
+            });
+
+            if (error) {
+                return {
+                    success: false,
+                    error,
+                };
+            }
+
+            if (data) {
+                return {
+                    success: true,
+                    redirectTo: "/",
+                };
+            }
+        } catch (error: any) {
+            return {
+                success: false,
+                error,
+            };
+        }
+        return {
+            success: false,
+            error: {
+                message: "Update password failed",
+                name: "Invalid password",
+            },
+        };
+    },
+    logout: async () => {
+        const { error } = await supabaseClient.auth.signOut();
+
+        if (error) {
+            return {
+                success: false,
+                error,
+            };
+        }
+
+        return {
+            success: true,
+            redirectTo: "/login",
+        };
+    },
+    onError: async (error) => {
+        console.error(error);
+        return { error };
+    },
+    check: async () => {
+        try {
+            const { data } = await supabaseClient.auth.getSession();
+            const { session } = data;
+
+            if (!session) {
+                return {
+                    authenticated: false,
+                    error: {
+                        message: "Check failed",
+                        name: "Session not found",
+                    },
+                    logout: true,
+                    redirectTo: "/login",
+                };
+            }
+        } catch (error: any) {
+            return {
+                authenticated: false,
+                error: error || {
+                    message: "Check failed",
+                    name: "Session not found",
+                },
+                logout: true,
+                redirectTo: "/login",
+            };
+        }
+
+        return {
+            authenticated: true,
+        };
+    },
+    getPermissions: async () => {
+        const user = await supabaseClient.auth.getUser();
+
+        if (user) {
+            return user.data.user?.role;
+        }
+
+        return null;
+    },
+    getIdentity: async () => {
+        const { data } = await supabaseClient.auth.getUser();
+
+        if (data?.user) {
+            return {
+                ...data.user,
+                name: data.user.email,
+            };
+        }
+
+        return null;
+    },
+};
+
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <GitHubBanner />
+            <ConfigProvider theme={RefineThemes.Blue}>
+                <Refine
+                    dataProvider={dataProvider(supabaseClient)}
+                    liveProvider={liveProvider(supabaseClient)}
+                    routerProvider={routerProvider}
+                    authProvider={authProvider}
+                    resources={[
+                        {
+                            name: "posts",
+                            list: "/posts",
+                            create: "/posts/create",
+                            edit: "/posts/edit/:id",
+                            show: "/posts/show/:id",
+                            meta: {
+                                canDelete: true,
+                            },
+                        },
+                    ]}
+                    notificationProvider={notificationProvider}
+                    /**
+                     * Multiple subscriptions are currently not supported with the supabase JS client v2 and @refinedev/supabase v4.
+                     * Therefore, enabling global live mode will cause unexpected behaviors.
+                     * Please set `liveMode: "auto"` or `liveMode: "manual"` manually while using real-time features of refine.
+                     */
+                    options={{
+                        liveMode: "off",
+                        syncWithLocation: true,
+                        warnWhenUnsavedChanges: true,
+                    }}
+                >
+                    <Routes>
+                        <Route
+                            element={
+                                <Authenticated
+                                    fallback={<CatchAllNavigate to="/login" />}
+                                >
+                                    <ThemedLayoutV2>
+                                        <Outlet />
+                                    </ThemedLayoutV2>
+                                </Authenticated>
+                            }
+                        >
+                            <Route
+                                index
+                                element={
+                                    <NavigateToResource resource="posts" />
+                                }
+                            />
+
+                            <Route path="/posts">
+                                <Route index element={<PostList />} />
+                                <Route path="create" element={<PostCreate />} />
+                                <Route path="edit/:id" element={<PostEdit />} />
+                                <Route path="show/:id" element={<PostShow />} />
+                            </Route>
+                        </Route>
+
+                        <Route
+                            element={
+                                <Authenticated fallback={<Outlet />}>
+                                    <NavigateToResource resource="posts" />
+                                </Authenticated>
+                            }
+                        >
+                            <Route
+                                path="/login"
+                                element={
+                                    <AuthPage
+                                        type="login"
+                                        providers={[
+                                            {
+                                                name: "google",
+                                                label: "Sign in with Google",
+                                                icon: (
+                                                    <GoogleOutlined
+                                                        style={{
+                                                            fontSize: 18,
+                                                            lineHeight: 0,
+                                                        }}
+                                                    />
+                                                ),
+                                            },
+                                        ]}
+                                        formProps={{
+                                            initialValues: {
+                                                email: "info@refine.dev",
+                                                password: "refine-supabase",
+                                            },
+                                        }}
+                                    />
+                                }
+                            />
+                            <Route
+                                path="/register"
+                                element={<AuthPage type="register" />}
+                            />
+                            <Route
+                                path="/forgot-password"
+                                element={<AuthPage type="forgotPassword" />}
+                            />
+                            <Route
+                                path="/update-password"
+                                element={<AuthPage type="updatePassword" />}
+                            />
+                        </Route>
+
+                        <Route
+                            element={
+                                <Authenticated>
+                                    <ThemedLayoutV2>
+                                        <Outlet />
+                                    </ThemedLayoutV2>
+                                </Authenticated>
+                            }
+                        >
+                            <Route path="*" element={<ErrorComponent />} />
+                        </Route>
+                    </Routes>
+                    <UnsavedChangesNotifier />
+                    <DocumentTitleHandler />
+                </Refine>
+            </ConfigProvider>
+        </BrowserRouter>
+    );
+};
+
+export default App;
